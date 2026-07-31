1aa8abfe01c5f9f517079a02ac5d0e8c1bdc507b
kmfxea <kmfxea@gmail.com>
2026-07-31 19:12:32 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = "marksup-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
