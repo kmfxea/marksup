0d3d15484dd4b9e86945744671b7d9e78c379c75
kmfxea <kmfxea@gmail.com>
2026-07-31 19:52:25 +0800

    Delete utils directory

--- utils/supabase_client.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from supabase import create_client, Client
-
-@st.cache_resource
-def get_supabase() -> Client:
-    url = st.secrets["supabase"]["url"]
-    key = st.secrets["supabase"]["key"]
-    return create_client(url, key)
